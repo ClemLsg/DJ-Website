295fa40f001883e0a3db5b05c483ddf0bc85fc02
LeVeloute <paul.fontaine@viacesi.fr>
2018-11-19 06:40:46 -0800

    fix this chit

--- resources/views/dj-rouen.blade.php
+++ resources/views/dj-rouen.blade.php
@@ -120,6 +120,8 @@
                             <p class="white-color">Nous sommes à votre service pour faire de vos soirées (anniversaire, mariage, ect...) un moment inoubliable sur {{$ville->name}} et partout en normandie.</p>
                             <a href="#service" class="btn border">Nos prestations sur {{$ville->name}}</a>
                             <a href="#contact" class="btn">Devis gratuit</a>
+                            <br>
+
                             <!-- TrustBox widget - Micro Review Count -->
                             <div class="trustpilot-widget" data-locale="fr-FR" data-template-id="5419b6a8b0d04a076446a9ad" data-businessunit-id="5beecfcfd3db7b0001a0e67b" data-style-height="24px" data-style-width="100%" data-theme="dark">
                                 <a href="https://fr.trustpilot.com/review/djiin.fr" target="_blank">Trustpilot</a>
